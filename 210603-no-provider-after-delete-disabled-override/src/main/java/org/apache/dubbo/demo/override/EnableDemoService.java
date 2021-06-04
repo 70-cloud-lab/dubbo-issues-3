28aed7a07e82332bba268dc8b15911af89c26f39
张远征hd <zhangyzhd@163.com>
2021-06-04 09:19:41 +0800

    add 210603-no-provider-after-delete-disabled-override

---
+++ 210603-no-provider-after-delete-disabled-override/src/main/java/org/apache/dubbo/demo/override/EnableDemoService.java
@@ -0,0 +1,17 @@
+package org.apache.dubbo.demo.override;
+
+import org.apache.dubbo.common.URL;
+import org.apache.dubbo.common.extension.ExtensionLoader;
+import org.apache.dubbo.registry.Registry;
+import org.apache.dubbo.registry.RegistryFactory;
+
+import java.io.IOException;
+
+public class EnableDemoService {
+    public static void main(String[] args) throws IOException {
+        RegistryFactory factory = ExtensionLoader.getExtensionLoader(RegistryFactory.class).getAdaptiveExtension();
+        Registry registry = factory.getRegistry(URL.valueOf("zookeeper://127.0.0.1:2181"));
+        registry.unregister(URL.valueOf("override://0.0.0.0/org.apache.dubbo.demo.DemoService?category=configurators&dynamic=false&disabled=true&enabled=true"));
+        System.in.read();
+    }
+}
